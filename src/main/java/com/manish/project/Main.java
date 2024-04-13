40ae4ea55bdd7743afbce06c61c3175a1099bac4
manish <ms5978307@gmail.com>
2024-04-13 23:17:36 +0530

    reset

--- src/main/java/com/manish/project/Main.java
+++ src/main/java/com/manish/project/Main.java
@@ -1,9 +1,6 @@
 package com.manish.project;
 
-import com.manish.project.spritestrom.GameEngine;
-
 public class Main {
     public static void main(String[] args) {
-        new GameEngine();
     }
 }
